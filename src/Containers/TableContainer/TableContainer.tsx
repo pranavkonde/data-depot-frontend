91553437746b2880bf78a97e355a48311611c086
arpit <arpit@lighthouse.storage>
2023-03-07 20:38:25 +0530

    changes to UI

--- src/Containers/TableContainer/TableContainer.tsx
+++ src/Containers/TableContainer/TableContainer.tsx
@@ -3,12 +3,13 @@ import "./TableContainer.scss";
 
 function TableContainer() {
   return (
-    <div className="TableContainer _card">
-      <table>
+    <div className="TableContainer _card ">
+      <table className="">
         <thead>
           <tr>
             <th>File ID</th>
             <th>File Name</th>
+            <th>File Size</th>
             <th>Status</th>
             <th>Upload Date</th>
             <th>Expiry Date</th>
@@ -18,13 +19,52 @@ function TableContainer() {
         </thead>
         <tbody>
           <tr>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
+            <td>#3066</td>
+            <td>Abcd</td>
+            <td>
+                <span className="fileSize">5GB</span>
+            </td>
+            <td>
+                <span className="fileSize">Active</span>
+            </td>
+            <td>Jan 6,2023</td>
+            <td>March 1,2023</td>
+            <td>url.bafybeih5zjybczcbaj4</td>
+            <td>
+                <span className="download">Download</span>
+            </td>
+          </tr>
+          <tr>
+            <td>#3066</td>
+            <td>Abcd</td>
+            <td>
+                <span className="fileSize">5GB</span>
+            </td>
+            <td>
+                <span className="fileSize">Active</span>
+            </td>
+            <td>Jan 6,2023</td>
+            <td>March 1,2023</td>
+            <td>url.bafybeih5zjybczcbaj4</td>
+            <td>
+                <span className="download">Download</span>
+            </td>
+          </tr>
+          <tr>
+            <td>#3066</td>
+            <td>Abcd</td>
+            <td>
+                <span className="fileSize">5GB</span>
+            </td>
+            <td>
+                <span className="fileSize">Active</span>
+            </td>
+            <td>Jan 6,2023</td>
+            <td>March 1,2023</td>
+            <td>url.bafybeih5zjybczcbaj4</td>
+            <td>
+                <span className="download">Download</span>
+            </td>
           </tr>
         </tbody>
       </table>
