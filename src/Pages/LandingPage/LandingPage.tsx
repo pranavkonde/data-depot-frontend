6efd0e33f6ae875d046a865aa84db2068aa55ac0
arpit <arpit@lighthouse.storage>
2023-03-13 03:33:58 +0530

    changes

--- src/Pages/LandingPage/LandingPage.tsx
+++ src/Pages/LandingPage/LandingPage.tsx
@@ -3,33 +3,40 @@ import { Footer, Header } from "../../Components";
 import { HeroSection, LoginDialog } from "../../Containers";
 import "./LandingPage.scss";
 import { Dialog } from "@material-ui/core";
-import { useLocation } from 'react-router-dom'
+import { useLocation } from "react-router-dom";
 import { login } from "../../Utils/Services/Auth";
 
-
 function LandingPage() {
-  const [loginDialog , setLoginDialog] = useState(false);
-    const location = useLocation();
-  
+  const [loginDialog, setLoginDialog] = useState(false);
+  const location = useLocation();
+
   useEffect(() => {
-    if(location?.pathname === '/login'){
+    if (location?.pathname === "/login") {
       const urlParams = new URLSearchParams(location?.search);
       const codeParam = urlParams.get("code");
       console.log(codeParam);
-      codeParam && (async ()=>{await login(codeParam)})()
+      codeParam &&
+        (async () => {
+          await login(codeParam);
+        })();
     }
-    return () => {}
-  }, [location])
-  
+    return () => {};
+  }, [location]);
+
   return (
     <div className="LandingPage">
       <Header setLoginDialog={setLoginDialog} />
-      <HeroSection />
-      <Footer/>
-      <Dialog open={loginDialog} onClose={() => {
-        setLoginDialog(false);
-      }}>
-        <LoginDialog/>
+      <div className="LandingPage__contentContainer">
+        <HeroSection />
+      </div>
+      <Footer />
+      <Dialog
+        open={loginDialog}
+        onClose={() => {
+          setLoginDialog(false);
+        }}
+      >
+        <LoginDialog />
       </Dialog>
     </div>
   );
